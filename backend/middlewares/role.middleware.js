10ef4075ab78c6d2d7abec22109e9c475f6b3d43
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-03-15 03:57:58 -0600

    Create role.middleware.js

---
+++ backend/middlewares/role.middleware.js
@@ -0,0 +1,14 @@
+export const checkRole = (roles) => {
+
+    return (req, res, next) => {
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                message: "No autorizado"
+            });
+        }
+
+        next();
+    };
+
+};
