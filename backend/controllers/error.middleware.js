b0e98d5b4f087b4a3c0046c19febab18c8de67ea
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-03-15 03:52:26 -0600

    Create error.middleware.js

---
+++ backend/controllers/error.middleware.js
@@ -0,0 +1,11 @@
+export const errorHandler = (err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+
+    console.error("ERROR:", err);
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Error interno del servidor"
+    });
+};
