0d0e60987385f2e03ed04ddf1468bb4ec6514947
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-03-15 04:00:20 -0600

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,56 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller.js";
+import { body } from "express-validator";
 
 const router = Router();
+router.post(
+"/register",
 
-router.post("/register", register);
-router.post("/login", login);
+body("email")
+.isEmail()
+.withMessage("Email inválido")
+.normalizeEmail(),
+
+body("password")
+.isLength({ min: 6 })
+.withMessage("La contraseña debe tener al menos 6 caracteres")
+.trim(),
+
+body("nombre")
+.notEmpty()
+.withMessage("El nombre es obligatorio")
+.trim()
+.escape(),
+
+body("direccion")
+.notEmpty()
+.withMessage("La dirección es obligatoria")
+.trim()
+.escape(),
+
+body("medidor")
+.notEmpty()
+.withMessage("El medidor es obligatorio")
+.trim()
+.escape(),
+
+register
+);
+
+router.post(
+"/login",
+
+body("email")
+.isEmail()
+.withMessage("Email inválido")
+.normalizeEmail(),
+
+body("password")
+.isLength({ min: 6 })
+.withMessage("La contraseña debe tener al menos 6 caracteres")
+.trim(),
+
+login
+);
 
 export default router;
