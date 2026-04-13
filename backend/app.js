01a1e0d61429d371c89c9b962afa1fc05a0d2ed7
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-04-13 01:28:18 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,59 +1,50 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path"; 
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import adminRoutes from "./routes/admin.routes.js";
+import contentRoutes from "./routes/content.routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
 app.use(
   cors({
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
-      const allowed = process.env.FRONTEND_URL || '';
-      const isLocal517 = origin.startsWith('http://localhost:517');
-      const isLocal3010 = origin.startsWith('http://localhost:3010');
-      const isRailway = origin.startsWith('https://dwp-equipo4-production.up.railway.app');
+      const allowed = process.env.FRONTEND_URL || "";
+      const isLocal517 = origin.startsWith("http://localhost:517");
+      const isLocal3010 = origin.startsWith("http://localhost:3010");
+      const isRailway = origin.startsWith("https://dwp-equipo4-production.up.railway.app");
+
       if (origin === allowed || isLocal517 || isLocal3010 || isRailway) {
         return callback(null, true);
       }
-      console.warn('CORS blocked origin', origin);
-      return callback(new Error('Not allowed by CORS'));
+
+      console.warn("CORS blocked origin", origin);
+      return callback(new Error("Not allowed by CORS"));
     },
-    credentials: true,
+    credentials: true
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/api", contentRoutes);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get(/^(?!\/api).+/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.use(errorHandler);
-
-
-
-const PORT = process.env.PORT || 3010;
-console.log("Inicio del servidor con variables:", {
-  PORT,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_NAME: process.env.DB_NAME,
-  FRONTEND_URL: process.env.FRONTEND_URL,
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
 
 export default app;
