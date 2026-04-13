326585bd8cb92d6a0922b5d4a37a84c4bfda49e2
josmar299 <a3522110243@alumno.uttehuacan.edu.mx>
2026-04-13 01:26:50 -0600

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,18 +1,6 @@
 CREATE DATABASE IF NOT EXISTS dwp4_app;
 USE dwp4_app;
 
--- CREATE TABLE users (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(100) NOT NULL,
---     email VARCHAR(100) NOT NULL UNIQUE,
---     address VARCHAR(255) NOT NULL,
---     water_meter VARCHAR(50) NOT NULL UNIQUE,
---     password VARCHAR(255) NOT NULL,
---     role ENUM('user', 'admin') DEFAULT 'user',
---     is_active BOOLEAN DEFAULT TRUE,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -21,6 +9,7 @@ CREATE TABLE users (
     water_meter VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role ENUM('user', 'admin') DEFAULT 'user',
+    status ENUM('active', 'banned') DEFAULT 'active',
     is_active BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -37,8 +26,36 @@ CREATE TABLE refresh_tokens (
 CREATE TABLE password_reset_tokens (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    token_hash VARCHAR(255) NOT NULL, -- Aquí se guarda el token cifrado
-    expires_at DATETIME NOT NULL,     -- Para controlar la expiración (investigación)
+    token_hash VARCHAR(255) NOT NULL,
+    expires_at DATETIME NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE announcements (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE reports (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    description TEXT NOT NULL,
+    status ENUM('Pendiente', 'En Proceso', 'Atendido') DEFAULT 'Pendiente',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE schedules (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tips (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
